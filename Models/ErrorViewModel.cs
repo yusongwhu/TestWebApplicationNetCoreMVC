4aea5800bb3bdbad17b2bc7a4e02b5d8ee5ceefc
yusongwhu <yusongwhu@gmail.com>
2025-07-25 10:22:05 -0400

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TestWebApplicationNetCoreMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
